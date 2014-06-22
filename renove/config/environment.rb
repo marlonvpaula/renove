dcc5458be431c402507228683b27a4a2df768475
Marlon <marlonvpaula@gmail.com>
2014-06-21 22:18:40 -0300

    Adicionando projeto Ruby

---
+++ renove/config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Renove::Application.initialize!
---
+++ renove/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Renove::Application.config.secret_key_base = '9ecaad0bff8f7f91a923d8cc1b8d0a9d9a57f01445b32092f077ac382cd7847d2abac228c5bfcb4cc104514915aefd34f532c8bafb8e3602a4dd516f72164118'
