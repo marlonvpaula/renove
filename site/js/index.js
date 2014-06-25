e3da9a0784944bb2122ee9c8b4c1c61050163073
Marlon <marlonvpaula@gmail.com>
2014-06-25 00:41:57 -0300

    Alteracao site Renove

---
+++ site/js/index.js
@@ -0,0 +1,24 @@
+!function ($) {
+    $(function(){
+      // carousel demo
+      $('#myCarousel').carousel({
+      	interval: 0
+      });
+    })
+
+    $(function(){
+	  // carousel demo
+	  $('#myCarouselSpan4').carousel();
+	})
+
+	$(function(){
+	  // carousel demo
+	  $('#myCarouselSpan5').carousel()
+	})
+
+	$(function(){
+	  // carousel demo
+	  $('#myCarouselSpan6').carousel()
+	})
+}(window.jQuery)
+
